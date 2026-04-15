14f3b7df99efbb78cbe4f49c33a8ed2b4f854a6d
Anthony Barros <cybertechsolucoestecnologicas@gmail.com>
2026-04-15 11:36:11 -0300

    sistema limpo funcionando

--- server.js
+++ server.js
@@ -9,17 +9,30 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// TESTE SIMPLES
+// ROTAS
+
 app.get("/", (req, res) => {
-    res.send("🔥 SERVIDOR ONLINE FUNCIONANDO");
+    res.render("dashboard");
 });
 
-app.get("/teste", (req, res) => {
-    res.send("OK TESTE");
+app.get("/pdv", (req, res) => {
+    res.render("pdv");
+});
+
+app.get("/produtos", (req, res) => {
+    res.render("produtos");
+});
+
+app.get("/caixa", (req, res) => {
+    res.render("caixa");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("🚀 SERVER OK");
+    console.log("🚀 SISTEMA RODANDO");
 });
